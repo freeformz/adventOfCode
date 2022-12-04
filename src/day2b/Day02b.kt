af089e5e89c123082409d5a60bef7f458d6c136c
Edward Muller <emuller@fastly.com>
2022-12-03 19:17:33 -0800

    use more enums

--- src/day2b/Day02b.kt
+++ src/day2b/Day02b.kt
@@ -2,18 +2,31 @@ package day2b
 
 import readInput
 
-enum class Play(val value: Int) {
-    Rock(1) {
+enum class Outcome(val input: Char, val value: Int) {
+    Lose('X', 0) {
+        override fun score(op: Play): Int = op.winsAgainst().value + value
+    },
+    Draw('Y', 3){
+        override fun score(op: Play): Int = op.value + value
+    },
+    Win('Z', 6){
+        override fun score(op: Play): Int = op.loosesAgainst().value + value
+    };
+
+    abstract fun score(op: Play): Int
+}
+enum class Play(val value: Int, val opInput: Char) {
+    Rock(1,'A') {
         override fun beats(o: Play) = o == Rock.winsAgainst()
         override fun winsAgainst() = Scissors
         override fun loosesAgainst(): Play = Paper
     },
-    Paper(2) {
+    Paper(2, 'B') {
         override fun beats(o: Play) = o == Paper.winsAgainst()
         override fun winsAgainst() = Rock
         override fun loosesAgainst(): Play = Scissors
     },
-    Scissors(3) {
+    Scissors(3, 'C') {
         override fun beats(o: Play) = o == Scissors.winsAgainst()
         override fun winsAgainst() = Paper
         override fun loosesAgainst(): Play = Rock
@@ -23,30 +36,15 @@ enum class Play(val value: Int) {
     abstract fun beats(o: Play): Boolean
 }
 
-val win = 6
-val draw = 3
-
 fun main() {
-    val sum = readInput("day2").sumOf {
-        val op = when (it.first()) {
-            'A' -> Play.Rock
-            'B' -> Play.Paper
-            'C' -> Play.Scissors
-            else -> throw Exception("unknown input")
+    val sum = readInput("day2").sumOf {input ->
+        val op = Play.values().first { pv ->
+            pv.opInput == input.first()
         }
-        val me = when (it.last()) {
-            'X' -> op.winsAgainst() // I need to loose
-            'Y' -> op
-            'Z' -> op.loosesAgainst() // I need to win
-            else -> throw Exception("unknown input")
+        val outcome = Outcome.values().first { out ->
+            out.input == input.last()
         }
-        if (me == op) {
-            me.value + draw
-        } else if (me.beats(op)) {
-            me.value + win
-        } else {
-            me.value
-        }
+        outcome.score(op)
     }
 
     println(sum)
