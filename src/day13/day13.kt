b31767b95e23e2af8651d26529e3c135db06b958
Edward Muller <me@freeformz.me>
2023-12-02 14:14:18 -0800

    old

---
+++ src/day13/day13.kt
@@ -0,0 +1,31 @@
+package day13
+
+import readInput
+
+fun main() {
+    fun parseInput(input: List<String>) {}
+
+    fun partOne(input: List<String>) {
+        parseInput(input)
+    }
+
+    fun partTwo(input: List<String>) {
+        parseInput(input)
+    }
+
+    println("partOne test")
+    partOne(readInput("day13.test"))
+    println()
+    /*
+        println("partOne")
+        partOne(readInput("day13"))
+        println()
+
+        println("partTwo with test input")
+        partTwo(readInput("day13.test"))
+        println()
+
+        println("partTwo")
+        partTwo(readInput("day13"))
+     */
+}
